6073670111a2fe727ce9dd24d2dbb32e7fa9e74e
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-12-21 13:06:09 +0530

    Fix text center

--- src/components/Thala.vue
+++ src/components/Thala.vue
@@ -1,11 +1,15 @@
 <template>
   <img src="../assets/thala.gif" alt="" />
-  <p class="text-2xl">Yayy! Thala For a Reason 😤</p>
+  <p class="text-2xl text-center">Yayy! Thala For a Reason 😤</p>
   <audio ref="audioPlayer">
     <source :src="songUrl" type="audio/mpeg" />
   </audio>
 
-  <a href="/" class="text-gray-900 font-medium bg-white border border-gray-300 hover:bg-gray-100 rounded text-sm px-4 py-2">Try Again</a>
+  <a
+    href="/"
+    class="text-gray-900 font-medium bg-white border border-gray-300 hover:bg-gray-100 rounded text-sm px-4 py-2"
+    >Try Again</a
+  >
 </template>
 
 <script setup>
