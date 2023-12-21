dab3774b238814960c18382ee4a08affef2d715c
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-12-21 12:54:16 +0530

    Add github repo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white h-12 shadow flex justify-between items-center px-10">
     <a href="/" class="font-semibold"> Thala Checker </a>
-    <a href="">GitHub</a>
+    <a href="https://github.com/sayakongit/thala-for-a-reason">GitHub</a>
   </div>
 </template>
 
